7871bdae46002ec6de4c352444d9e39104c693e3
Sumit Gond <sumitgond8745@gmail.com>
2024-02-08 22:38:11 +0530

    Big O Intro Completed

---
+++ JavaIntro/src/com/dsa/pointer/Pointer.java
@@ -0,0 +1,31 @@
+package com.dsa.pointer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pointer {
+
+	public static void main(String[] args) {
+		
+	//Case : 1 --> Interger and int
+		int num1 = 11;
+		int num2 = num1;
+		
+		num1 = 22; // after
+		
+//		System.out.println("num1 : " + num1);
+//		System.out.println("num2 : " + num2);
+		
+		//Case : 2 --> Map
+		
+		Map<String, Integer> map1 = new HashMap<>();
+		Map<String, Integer> map2 = new HashMap<>();
+		map1.put("Value", 11);
+		map2 = map1;
+		
+//		map1.put("value", 22); // after
+		
+		System.out.println("map1 : " + map1);
+		System.out.println("map2 : " + map2);
+	}
+}
