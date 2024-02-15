a2a5df0e16ba855ee4d638d8ee83949066360b5e
Sumit Gond <sumitgond8745@gmail.com>
2024-02-15 18:55:47 +0530

    DLL completed

--- DoublyLinkedList/src/com/doublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/src/com/doublyLinkedList/DoublyLinkedList.java
@@ -108,4 +108,77 @@ public class DoublyLinkedList {
 		return temp;
 	}
 
+//Get The Node of a particular Node
+	public Node get(int index) {
+		if (index < 0 && index >= length) {
+			return null;
+		}
+		Node temp = head;
+		if (index < length / 2) {
+			for (int i = 0; i < index; i++) {
+				temp = temp.next;
+			}
+		} else {
+			temp = tail;
+			for (int i = length - 1; i > index; i--) {
+				temp = temp.prev;
+			}
+		}
+		return temp;
+	}
+
+// Set a Node Value to a particular index
+	public boolean set(int index, int value) {
+		Node temp = get(index);
+		if (temp != null) {
+			temp.value = value;
+			return true;
+		}
+		return false;
+	}
+
+//Insert a Node at a particular index
+	public boolean insert(int index, int value) {
+		if (index < 0 || index >= length) {
+			return false;
+		}
+		if (index == 0) {
+			prepend(value);
+		}
+		if (index == length) {
+			append(value);
+		}
+		Node newNode = new Node(value);
+		Node before = get(index - 1);
+		Node after = before.next;
+		newNode.prev = before;
+		newNode.next = after;
+		before.next = newNode;
+		after.prev = newNode;
+		length++;
+		return true;
+	}
+
+//Remove a Node at a particular Index
+	public Node remove(int index) {
+		if (index < 0 || index >= length) {
+			return null;
+		}
+		if (index == 0) {
+			return removeFirst();
+		}
+		if (index == length - 1) {
+			return removeLast();
+		}
+
+		Node temp = get(index);
+		temp.next.prev = temp.prev;
+		temp.prev.next = temp.next;
+		temp.prev = null;
+		temp.next = null;
+		length--;
+		return temp;
+
+	}
+
 }
--- DoublyLinkedList/src/com/doublyLinkedList/MainDoubly.java
+++ DoublyLinkedList/src/com/doublyLinkedList/MainDoubly.java
@@ -8,6 +8,7 @@ public class MainDoubly {
 		doublyLL.append(4);
 		doublyLL.append(2);
 		doublyLL.append(9);
+		doublyLL.append(45);
 
 		doublyLL.getHead();
 		doublyLL.getTail();
@@ -24,11 +25,32 @@ public class MainDoubly {
 //		System.out.println("Last Node from the Doubly Linked List is : " + doublyLL.removeLast());
 
 //Prepend
-		doublyLL.prepend(5);
-		doublyLL.printList();
+//		doublyLL.prepend(5);
+//		doublyLL.printList();
 
-		System.out.println("First Node from the Doubly Linked List is : " + doublyLL.removeFirst().value);
-		System.out.println("First Node from the Doubly Linked List is : " + doublyLL.removeFirst().value);
+// Remove First Node from the DLL
+//		System.out.println("First Node from the Doubly Linked List is : " + doublyLL.removeFirst().value);
+//		System.out.println("First Node from the Doubly Linked List is : " + doublyLL.removeFirst().value);
+
+//Get Node at a particular index
+//		System.out.println("Node at a particular index : " + doublyLL.get(3).value);
+
+// Set a Node value to a particular index
+//		doublyLL.set(2, 3);
+//		doublyLL.printList();
+//		System.out.println();
+
+//Set a Node at a Particular index
+//		doublyLL.insert(3, 65);
+//		doublyLL.getHead();
+//		doublyLL.getTail();
+//		doublyLL.getLength();
+//		doublyLL.printList();
+
+//Remove a Node at a particular Index
+		System.out.println("Removing Node at a particular index and value is : " + doublyLL.remove(2).value);
+		doublyLL.getLength();
+		doublyLL.printList();
 
 	}
 
